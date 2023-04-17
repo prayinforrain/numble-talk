c6e5426c933e2a137b2de31d1a4685d937002b92
PrayinForRain <46566891+prayinforrain@users.noreply.github.com>
2023-04-17 18:00:31 +0900

    💎 style: eslint 재적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
+import styled from '@emotion/styled';
 import type { AppProps } from 'next/app';
 
 import NumbleProvider from '@/components/common/Theme';
-import styled from '@emotion/styled';
 import { COLOR } from '@/constants/color';
 
 export default function App({ Component, pageProps }: AppProps) {
